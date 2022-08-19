4457f05423d3380547992160f236f11da8e7b73c
unknown <hayounn.etd@gmail.com>
2022-08-19 19:06:36 +0100

    Add enumerable function

--- my_list.rb
+++ my_list.rb
@@ -13,4 +13,13 @@ class MyList
     end
 end
 
+# Create our list
 
+list = MyList.new(1, 2, 3, 4)
+
+
+puts(list.all? {|e| e < 5})
+puts(list.all? {|e| e > 5})
+puts(list.any? {|e| e == 2})
+puts(list.any? {|e| e == 5})
+puts(list.filter {|e| e.even?})
